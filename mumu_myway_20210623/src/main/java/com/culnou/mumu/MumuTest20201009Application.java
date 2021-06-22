ddf54b826e4664630302951320cfe778575310b1
“rakusui” <rakusui@culnou.com>
2021-06-23 08:25:58 +0900

    初回コミット。

---
+++ mumu_myway_20210623/src/main/java/com/culnou/mumu/MumuTest20201009Application.java
@@ -0,0 +1,13 @@
+package com.culnou.mumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumuTest20201009Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumuTest20201009Application.class, args);
+	}
+
+}
---
+++ mumu_myway_20210623/src/test/java/com/culnou/mumu/MumuTest20201009ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.culnou.mumu;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MumuTest20201009ApplicationTests {
+
+	public MumuTest20201009ApplicationTests() {
+		
+	}
+	@org.junit.Test
+	public void contextLoads() {
+	}
+
+}
